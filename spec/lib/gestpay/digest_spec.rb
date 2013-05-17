3b43af4e371ddd42c476197763ef893517893ede
Alessandro Mencarini <a.mencarini@freegoweb.it>
2013-05-17 11:59:10 +0200

    Other Spec correction for 1.8.7

--- spec/lib/gestpay/digest_spec.rb
+++ spec/lib/gestpay/digest_spec.rb
@@ -1,4 +1,4 @@
-require (File.expand_path('./../../spec_helper', __FILE__))
+require (File.expand_path('../../../spec_helper', __FILE__))
 # For Ruby > 1.9.3, use this instead of require
 # require_relative '../../spec_helper'
 
