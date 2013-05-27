993cdaef2a573d9d3838d968c80e276f832c5915
Alessandro Mencarini <a.mencarini@freegoweb.it>
2013-05-27 18:06:46 +0200

    Added transaction_key to verify visa exception

--- lib/gestpay/error/verify_visa.rb
+++ lib/gestpay/error/verify_visa.rb
@@ -5,6 +5,7 @@ module Gestpay
       attr_accessor :result, :crypted_response
 
       def initialize(info)
+        @transaction_key = info[:transaction_key]
         @result = info[:vb_v][:vb_v_buyer]
         @crypted_response = info[:vb_v][:vb_v_risp]
       end
--- spec/lib/gestpay/gateway_spec.rb
+++ spec/lib/gestpay/gateway_spec.rb
@@ -2,7 +2,7 @@ require (File.expand_path('../../../spec_helper', __FILE__))
 # For Ruby > 1.9.3, use this instead of require
 # require_relative '../../spec_helper'
 
-describe Gestpay::Digest do
+describe Gestpay::Gateway do
 
   let(:gateway) { Gestpay::Gateway.new }
 
