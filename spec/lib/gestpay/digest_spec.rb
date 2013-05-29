681b6901a249c0c65ed6beec79d7cdcc1433b1a9
Alessandro Mencarini <a.mencarini@freegoweb.it>
2013-05-29 12:02:03 +0200

    Refactored tests

--- spec/lib/gestpay/digest_spec.rb
+++ spec/lib/gestpay/digest_spec.rb
@@ -5,6 +5,7 @@ require (File.expand_path('../../../spec_helper', __FILE__))
 describe Gestpay::Digest do
 
   let(:digest) { Gestpay::Digest.new }
+  subject { result }
 
   after do
     VCR.eject_cassette
@@ -26,46 +27,36 @@ describe Gestpay::Digest do
         }
       end
 
-      it 'should return a Result object' do
-        result.should respond_to(:success?)
-      end
-
-      it "should return a Result with a token" do
-        result.should be_success
-        result.encrypted_string.should == 'i3eUhxjb5HNxldtVSXb4qxaYva_uWKNn6BSQgRXkBUWKjoQvU6IbNgocbJYSr2LZ3FdnsvnpXH8KQ1*lizVn5LmWYMmN7JA0wMpuURkqftlBYPdjzXMmOomRrUenCF1JzE0j1ugNdSIbY7rrrUVxYAtjdvkKmyKrtLOiDuTohJsWaWGuIH4y_KUUa5cWcY3Oqz8eSjVfiWr8RUeAYCVmeA'
-      end
+      it { should respond_to(:success?) }
+      it { should be_success }
+      its(:encrypted_string) { should include('i3eUhxjb5HNxldtVSXb4qxaYva_uWKNn6BSQgRXkBUWKjoQvU6IbNgocbJYSr2LZ3FdnsvnpXH8KQ1') }
     end
 
     context "with a wrong data hash" do
       before { VCR.insert_cassette 'encrypt_ko', :record => :new_episodes }
       let(:hash) { Hash.new }
 
-      it "should raise an error" do
-        result.should_not be_success
-      end
+      it { should_not be_success }
     end
   end
 
   describe '#decrypt' do
     context "with an ok transaction" do
       before { VCR.insert_cassette 'decrypt_ok', :record => :new_episodes }
-      it "should return an ok envelope" do
-        result = digest.decrypt('rxd4jKk3CN8PbPX7gHh9u60ocD8_R85bI0NR_C*SOk1r1y2P8E9fDBgXKmmMnyyhyosyF8X6rvyTs_GCulPAy8TZKJc99tSb57cAWuo36MZ9_OWp2pwgKYLx7jS9XaLBgAqBufiFIoPq*Pwj5SdHY06vUf3ebnKryDvRnC0oldLw1YxpkIxlebYd7YxvNfSGLfSvce2NN8wWcwPfdR2jPKb*_oJpCEVZNO5TVMaH6h1UONEksNLIuDeZPqWfrHRcgbi0MLuoifb7ZBGrTw7HlEqfF6ojKt2oihKtNdn3Y3W5MkBHCsBrWq63TLYlDU8PRaV20jAdqQIJBHopVyumMGtyLoHlMFaH*nXUvgzwmYnTtwQZ*Q1nGCP0cl4YilBcivSTJCOkcrRwUhqYOl*4F1kmT15Nkp_sBB4oJRODYs8')
-        result.should be_success
-        result.data[:buyer].should == {:buyer_name=>"Test Customer", :buyer_email=>"test@example.com"}
-        result.data[:transaction_result].should == 'OK'
-        result.data[:shop_transaction_id].should == '123'
-      end
+      let(:result ) { digest.decrypt('rxd4jKk3CN8PbPX7gHh9u60ocD8_R85bI0NR_C*SOk1r1y2P8E9fDBgXKmmMnyyhyosyF8X6rvyTs_GCulPAy8TZKJc99tSb57cAWuo36MZ9_OWp2pwgKYLx7jS9XaLBgAqBufiFIoPq*Pwj5SdHY06vUf3ebnKryDvRnC0oldLw1YxpkIxlebYd7YxvNfSGLfSvce2NN8wWcwPfdR2jPKb*_oJpCEVZNO5TVMaH6h1UONEksNLIuDeZPqWfrHRcgbi0MLuoifb7ZBGrTw7HlEqfF6ojKt2oihKtNdn3Y3W5MkBHCsBrWq63TLYlDU8PRaV20jAdqQIJBHopVyumMGtyLoHlMFaH*nXUvgzwmYnTtwQZ*Q1nGCP0cl4YilBcivSTJCOkcrRwUhqYOl*4F1kmT15Nkp_sBB4oJRODYs8') }
+
+      it { should be_success }
+      its(:buyer_name) { should == "Test Customer" }
+      its(:buyer_email) { should == "test@example.com" }
+      its(:shop_transaction_id) { should == '123' }
     end
 
     context "with a failed transaction" do
       before { VCR.insert_cassette 'decrypt_ko', :record => :new_episodes }
-      it "should return a ko envelope" do
-        string = 'KFzOej9MGr*lXEe*LOB2wzLmHja3ghKAo3iMpR1pG35moFdejWKzAj1Zd*R_e0x8qyOSTQmZVGRXEzJMVzd9xRXS*7tkbQLPOuBZ7qAtoFsX2Fxks5HxY*3YsUeiOInUXNF7*ih1bzkwmYGchDqYnEi31rN8ZYwLHnjwMIEmWJNn7oJHE6RpfUHG*7qohDxZRto*VVTPFdM93CwECq7SdYLebAQ_9EefdcE90QCRcFdGxZp_yAr_t*YRgLcZ5B5j843krtjCvf_m8nS99CroJQ768XsvlXy29McM2G*369mTufGyGMRab5PIvJlpSd3ds6mGi1IoMJAyXVkCu_ygRDDXN5Fk4Hz58LrDIci4ZszyVRcqy56SeYV7eG*kG8LZV40WV2ey8Qeh8XDUZnCV3A'
-        result = digest.decrypt(string)
-        result.should_not be_success
-        result.error.should == "Error 74: Autorizzazione negata"
-      end
+      let(:result ) { digest.decrypt('KFzOej9MGr*lXEe*LOB2wzLmHja3ghKAo3iMpR1pG35moFdejWKzAj1Zd*R_e0x8qyOSTQmZVGRXEzJMVzd9xRXS*7tkbQLPOuBZ7qAtoFsX2Fxks5HxY*3YsUeiOInUXNF7*ih1bzkwmYGchDqYnEi31rN8ZYwLHnjwMIEmWJNn7oJHE6RpfUHG*7qohDxZRto*VVTPFdM93CwECq7SdYLebAQ_9EefdcE90QCRcFdGxZp_yAr_t*YRgLcZ5B5j843krtjCvf_m8nS99CroJQ768XsvlXy29McM2G*369mTufGyGMRab5PIvJlpSd3ds6mGi1IoMJAyXVkCu_ygRDDXN5Fk4Hz58LrDIci4ZszyVRcqy56SeYV7eG*kG8LZV40WV2ey8Qeh8XDUZnCV3A') }
+
+      it { should_not be_success }
+      its(:error) { should == "Error 74: Autorizzazione negata" }
     end
   end
 
--- spec/lib/gestpay/gateway_spec.rb
+++ spec/lib/gestpay/gateway_spec.rb
@@ -5,16 +5,19 @@ require (File.expand_path('../../../spec_helper', __FILE__))
 describe Gestpay::Gateway do
 
   let(:gateway) { Gestpay::Gateway.new }
+  subject { result }
 
   after do
     VCR.eject_cassette
   end
 
   describe '#payment' do
+    let(:result) { gateway.payment(hash) }
+
     context "with an ok data hash" do
       before { VCR.insert_cassette 'payment_ok', :record => :new_episodes }
-      it "should return stuff" do
-        hash = {
+      let(:hash) do
+        {
           :amount              => '0.02',
           :shop_transaction_id => '12',
           :card_number         => '5364000000000987',
@@ -28,82 +31,74 @@ describe Gestpay::Gateway do
             :payment_id => 15
           }
         }
-        result = gateway.payment(hash)
-        result.should be_success
-        result.amount.should == 0.02
       end
+
+      it { should be_success }
+      its(:amount) { should == 0.02 }
+      its(:visa_encrypted_string) { should be_nil}
     end
 
     context "with a VISA credit card with Verification" do
       before { VCR.insert_cassette 'payment_visa', :record => :new_episodes }
-      it "should raise a VerifyVisa error" do
-        hash = {
-          :card_number => '4556541926187165'
-        }
-        result = gateway.payment(hash)
-        result.should be_verify_by_visa
+      let(:hash) do
+        { :card_number => '4556541926187165' }
       end
+
+      it { should be_verify_by_visa }
+      its(:visa_encrypted_string) { should include('mRTLDNghVNbEYcEsmpi5WTNwD5XIrf2NKbO8iVRXYd5we9*K*c8') }
     end
 
     context "with an invalid credit card" do
       before { VCR.insert_cassette 'payment_ko', :record => :new_episodes }
-      it "should raise an error" do
-        hash = {
+      let(:hash) do
+        {
           :amount              => '0.02',
           :shop_transaction_id => '12',
           :card_number         => '5432123456789012'
         }
-        result = gateway.payment(hash)
-        result.should_not be_success
-        result.error.should == 'Error 74: Autorizzazione negata'
       end
+
+      it { should_not be_success }
+      its(:error) { should == 'Error 74: Autorizzazione negata' }
     end
 
     context "with a token" do
       before { VCR.insert_cassette 'payment_token', :record => :new_episodes }
-      it "should be ok" do
-        hash = {
+      let(:hash) do
+        {
           :amount              => '0.02',
           :shop_transaction_id => '12',
           :token_value         => '53QWERTYU0I90987'
         }
-        result = gateway.payment(hash)
-        result.should be_success
-        result.amount.should == 0.02
       end
+      it { should be_success }
+      its(:amount) { should == 0.02 }
     end
 
   end
 
   describe '#request_token' do
+    let(:hash) do
+      {
+        :card_number  => '4556541926187165',
+        :expiry_month => '12',
+        :expiry_year  => '20',
+        :cvv         => "123",
+      }
+    end
+
     context "with an ok data hash" do
       before { VCR.insert_cassette 'request_token_ok', :record => :new_episodes }
-      it "should return a payment token" do
-        hash = {
-          :card_number  => '4556541926187165',
-          :expiry_month => '12',
-          :expiry_year  => '20',
-          :cvv         => "123",
-        }
-        # This returned ok as we didn't verify the card with the false parameter
-        result = gateway.request_token(hash, false)
-        result.token.should == "45OGBX64451Y7165"
-        # result[:info] also returned
-      end
+      let(:result) { gateway.request_token(hash, false) }
+
+      its(:token) { should == "45OGBX64451Y7165" }
     end
 
     context "with an invalid credit card" do
       before { VCR.insert_cassette 'request_token_ko', :record => :new_episodes }
-      it "should raise an error" do
-        hash = {
-          :card_number  => '4556541926187165',
-          :expiry_month => '12',
-          :expiry_year  => '20',
-          :cvv         => "123",
-        }
-        result = gateway.request_token(hash)
-        result.error.should == "Error 405: Autorizzazione negata dai circuiti"
-      end
+      let(:result) { gateway.request_token(hash) }
+
+      its(:error) { should == "Error 405: Autorizzazione negata dai circuiti" }
     end
   end
 
