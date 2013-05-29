5650003a9774dce4296993f52fd632cb5666560c
Alessandro Mencarini <a.mencarini@freegoweb.it>
2013-05-29 12:01:38 +0200

    Added helper methods for buyer info

--- lib/gestpay/result/decrypt.rb
+++ lib/gestpay/result/decrypt.rb
@@ -1,6 +1,12 @@
 module Gestpay
   module Result
     class Decrypt < Gestpay::Result::Base
+      def buyer_name
+        buyer[:buyer_name]
+      end
+      def buyer_email
+        buyer[:buyer_email]
+      end
     end
   end
 end
