945c2eea1292f5b2b7e6a293fe89a092d747934a
Alessandro Mencarini <a.mencarini@freegoweb.it>
2013-05-29 12:01:54 +0200

    Added helper method for Verify by Visa string

--- lib/gestpay/result/payment.rb
+++ lib/gestpay/result/payment.rb
@@ -8,6 +8,11 @@ module Gestpay
       def verify_by_visa?
         error_code == '8006'
       end
+
+      def visa_encrypted_string
+        verify_by_visa_data = data[:vb_v]
+        verify_by_visa_data[:vb_v_risp] if verify_by_visa? && verify_by_visa_data[:vb_v_buyer] == 'OK'
+      end
     end
   end
 end
